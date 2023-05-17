e9b606f78b28de2f801b94fe617240d66297a05d
TabbieManywanda <achiengtabitha@gmail.com>
2023-05-17 11:35:15 +0000

    Checking validity of password after hashing

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     which is a byte string'''
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''checks if password is valid and
+    returns a boolean'''
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
