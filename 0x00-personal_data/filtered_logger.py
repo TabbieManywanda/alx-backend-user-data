32f45e08b9e537bfb01278800c9a6481819797ac
TabbieManywanda <achiengtabitha@gmail.com>
2023-05-17 10:34:42 +0000

    returns a logging.Logger object.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,9 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -37,3 +40,16 @@ def filter_datum(fields: List[str], redaction: str,
                          f'{field}={redaction}{separator}',
                          message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    '''takes no arguments and returns a `logging.Logger object`'''
+
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+
+    return logger
